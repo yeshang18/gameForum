3eb7477f1fa68c1db110952f3770a9bade733ac8
yeshang18 <2500595148@qq.com>
2023-04-04 00:34:46 +0800

    新建

---
+++ src/main/java/com/gameForum/GameForumApplication.java
@@ -0,0 +1,13 @@
+package com.gameForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameForumApplication.class, args);
+    }
+
+}
