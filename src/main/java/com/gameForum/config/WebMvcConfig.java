e16378b08c179bb5aa0d698700def0adc42c08c3
yeshang18 <2500595148@qq.com>
2023-06-11 14:10:45 +0800

    v1.0完成

--- src/main/java/com/gameForum/config/WebMvcConfig.java
+++ src/main/java/com/gameForum/config/WebMvcConfig.java
@@ -27,8 +27,8 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-        registry.addResourceHandler("/images/**").addResourceLocations("file:D://gameforumImg/");
-//        registry.addResourceHandler("images/**").addResourceLocations("file:/www/wwwroot/default/GameServer/BBSImg/");
+//        registry.addResourceHandler("/images/**").addResourceLocations("file:D://gameforumImg/");
+        registry.addResourceHandler("images/**").addResourceLocations("file:/www/wwwroot/default/GameServer/BBSImg/");
     }
 
     /**
@@ -47,6 +47,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://118.89.199.233:8081")
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .allowedHeaders("*");
